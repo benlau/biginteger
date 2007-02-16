00c6448a6c5fc7a68427ca1dc3f5f523563e1041
Matt McCutchen <hashproduct@gmail.com>
2007-02-16 15:33:27 -0500

    - "Modernize" the documentation and sample program. - Wrap some text to 80-character lines. - Sample program: Fix the number of powers to compute so it can run without   user intervention.  And increase the base from 3 to 314 to show big numbers   without ridiculously long output.

--- sample.cc
+++ sample.cc
@@ -1,16 +1,8 @@
 /*
 * Matt McCutchen's Big Integer Library
-*/
-
-/*
-* This sample program demonstrates the most important features of the Big Integer Library.
-* To get started quickly, read the code and explanations below.  Then try the program out.
 *
-* If you want more detail or more speed or can't find a feature here, look in the
-* appropriate source file.  This file shows only the more ``user-friendly'' features;
-* the other features are messier but worth learning eventually.
-*
-* GO FORTH and play with many-digit numbers!  (c.f. The TeXbook.)
+* Sample program demonstrating the most important features of the Big
+* Integer Library
 */
 
 // Standard libraries
@@ -25,43 +17,47 @@
 
 int main() {
 	try {
-		std::cout << "=====\nBig Integer Library Demonstration" << std::endl;
-		
 		BigInteger a; // a is 0
 		int b = 535;
 		
 		a = b; // From int to BigInteger...
 		b = a; // ...and back, no casts required!
 		/*
-		* If a were too big for an int you'd get a runtime exception.  The Big Integer Library
-		* throws C-strings (that is, `const char *'s) when something goes wrong.  It's a good
-		* idea to catch them; the `try/catch' construct wrapping all this code is an example
-		* of how to do this.  Some C++ compilers need a special command-line option to compile
-		* code that uses exceptions.
+		* If a were too big for an int you'd get a runtime exception.
+		* The Big Integer Library throws C-strings (that is,
+		* `const char *'s) when something goes wrong.  It's a good idea
+		* to catch them; the `try/catch' construct wrapping all this
+		* code is an example of how to do this.  Some C++ compilers need
+		* a special command-line option to compile code that uses
+		* exceptions.
 		*/
 		
 		BigInteger c(a); // Copy a BigInteger.
 		
-		BigInteger d(-314159265); // c is -314159265.  The `int' literal is converted to a BigInteger.
+		// d is -314159265.  The `int' literal is converted to a
+		// BigInteger.
+		BigInteger d(-314159265);
 		
-		// Ahem: that's too big to be an `int' literal (or even a `long' literal)!
-		// Disillusion yourself now -- this won't compile.
+		// This won't compile because the number is too big to be an
+		// integer literal.
 		//BigInteger e(3141592653589793238462643383279);
 		
+		// Instead you can convert the number from a string.
 		std::string s("3141592653589793238462643383279");
 		BigInteger f = easyStringToBI(s);
-		// Ah.  The string is converted to a BigInteger, and strings can be as long as you want.
 		
-		std::string s2 = easyBItoString(f); // You can convert the other way too.
+		// You can convert the other way too.
+		std::string s2 = easyBItoString(f); 
 		
-		std::cout << f << std::endl; // f is stringified and send to std::cout.
+		// f is stringified and send to std::cout.
+		std::cout << f << std::endl;
 		
 		/*
 		* Let's do some math!
 		*
 		* The Big Integer Library provides lots of overloaded operators
 		* and corresponding assignment operators.  So you can do `a + b'
-		* with big integers just as with normal integers.  The named
+		* with BigIntegers just as with normal integers.  The named
 		* methods `add', `divideWithRemainder', etc. are more advanced
 		* ``put-here operations''; see `BigUnsigned.hh' for details.
 		*/
@@ -70,33 +66,22 @@ int main() {
 		std::cout << (g + h) << '\n' << (g - h) << '\n' << (g * h)
 			<< '\n' << (g / h) << '\n' << (g % h) << std::endl;
 		
-		std::cout << "=====\nTest code" << std::endl;
+		// Let's do some heavy lifting and calculate powers of 314.
+		int maxPower = 10;
+		BigUnsigned x(1), big314(314);
+		for (int power = 0; power <= maxPower; power++) {
+			std::cout << "314^" << power << " = " << x << std::endl;
+			x *= big314; // A BigInteger assignment operator
+		}
 		
 		/*
 		* If you want to experiment with the library,
-		* put your own test code here.
+		* you can add your own test code here.
 		*/
-		
-		/*
-		* (End of test code)
-		*/
-		
-		// Let's do some heavy lifting.
-		std::cout << "=====\nPowers of 3" << std::endl;
-		std::cout << "How many do you want?" << std::endl;
-		int maxPower;
-		std::cin >> maxPower;
-		
-		BigUnsigned x(1), three(3);
-		for (int power = 0; power <= maxPower; power++) {
-			std::cout << "3^" << power << " = " << x << std::endl;
-			x *= three; // A BigInteger assignment operator
-		}
-		
-		std::cout << "There you go.  Goodbye.\n=====" << std::endl;
+		// std::cout << "Beginning of custom test code:\n" << std::endl;
 		
 	} catch(char const* err) {
-		std::cout << "=====\nSorry, the library threw an exception:\n"
+		std::cout << "The library threw an exception:\n"
 			<< err << std::endl;
 	}
 	
@@ -104,7 +89,7 @@ int main() {
 }
 
 /*
-* Here is the output of a sample run of this sample program:
+Running the sample program produces this output:
 
 3141592653589793238462643383279
 314424
@@ -112,12 +97,16 @@ int main() {
 83252135
 1185
 134
-Powers of 3
-How many do you want?
-2
-3^0 = 1
-3^1 = 3
-3^2 = 9
-There you go.  Goodbye.
+314^0 = 1
+314^1 = 314
+314^2 = 98596
+314^3 = 30959144
+314^4 = 9721171216
+314^5 = 3052447761824
+314^6 = 958468597212736
+314^7 = 300959139524799104
+314^8 = 94501169810786918656
+314^9 = 29673367320587092457984
+314^10 = 9317437338664347031806976
 
 */
